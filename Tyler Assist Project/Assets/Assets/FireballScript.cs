121dc793c12e75ca161e9d5e4f12ff171ea60fae
nickronk <46404075+nickronk@users.noreply.github.com>
2023-03-09 20:28:32 -0500

    Summary (Yoshi)

--- Tyler Assist Project/Assets/Assets/FireballScript.cs
+++ Tyler Assist Project/Assets/Assets/FireballScript.cs
@@ -9,18 +9,21 @@ public class FireballScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(transform.forward*Time.deltaTime*2);
+        transform.Translate(transform.forward*Time.deltaTime*10);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
+        Debug.Log(1);
         //Instantiate(explosion);
         Collider[] colliders = Physics.OverlapSphere(transform.position, 5);
         foreach (Collider localCollider in colliders)
         {
             Rigidbody localRigid = localCollider.GetComponent<Rigidbody>();
+            
             if (localRigid!=null)
             {
+                if (localRigid.gameObject.tag!="Weapon")
                 localRigid.AddExplosionForce(1000, transform.position, 5);
             }
         }
